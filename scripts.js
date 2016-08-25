1c60500adee2648d4c229be8ff899bbb71a59386
octoadmin <info@octomarketing.onmicrosoft.com>
2016-08-25 10:04:15 -0600

    Added ability to change the min and max. Added functionality that adds 10 to max and removes 10 from min upon play again.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 var firstLine = document.getElementById('firstLine');
+var minGuessInput = document.getElementById('minGuessInput');
+var maxGuessInput = document.getElementById('maxGuessInput');
 var secondLine = document.getElementById('secondLine');
 var thirdLine = document.getElementById('thirdLine');
 var guessInput = document.getElementById('guessInput');
@@ -6,10 +8,15 @@ var guessButton = document.getElementById('guessButton');
 var clearButton = document.getElementById('clearButton');
 var resetButton = document.getElementById('resetButton');
 var errorPrompt = document.getElementById('errorPrompt');
+var playAgainButton = document.getElementById('playAgainButton');
+
 var minGuess = 0;
 var maxGuess = 100;
+var number = Math.round(Math.random()*101);
 
-number = Math.round(Math.random()*101);
+function generateRandomNumber() {
+  number = Math.round(Math.random()*((maxGuess - minGuess)+1));
+};
 
 function clearError() {
     errorPrompt.innerText = '';
@@ -22,58 +29,93 @@ function clearMessageLines() {
 
 guessButton.addEventListener('click', function () {
   var guess = parseInt(guessInput.value);
-  if (isNaN(guess)) {
-      errorPrompt.innerText = '*You need to guess a number';
-      clearMessageLines();
-  }
-  if (guess > number) {
+  if (guess > number && guess < maxGuess) {
       secondLine.innerText = 'You guessed ' + guess;
       thirdLine.innerText = 'Sorry, that guess is too high. Try a lower number.';
-      errorPrompt.innerText = '';
+      clearError();
   }
-  if (guess < number) {
+  if (guess < number && guess > minGuess) {
       secondLine.innerText = 'You guessed ' + guess;
       thirdLine.innerText = 'Sorry, that guess is too low. Try a higher number.';
-      errorPrompt.innerText = '';
+      clearError();
   }
   if (guess === number) {
       secondLine.innerText = 'You guessed ' + guess;
-      thirdLine.innerText = 'Congradulations! You guessed the number!';
-      errorPrompt.innerText = '';
+      thirdLine.innerText = 'Congratulations! You guessed the number!';
+      clearError();
+      playAgainButton.style.display = 'inline-block';
   }
   if (guess < minGuess) {
       errorPrompt.innerText = '*That number is below the minimum guess.';
-      clearMessageLines();
   }
   if (guess > maxGuess) {
       errorPrompt.innerText = '*That number is above the maximum guess';
-      clearMessageLines();
+  }
+  if (isNaN(guess)) {
+      errorPrompt.innerText = '*You need to guess a number';
   }
 });
 
+function clearGuessInput() {
+  guessInput.value = '';
+}
+
 clearButton.addEventListener('click', function () {
-    guessInput.value = '';
+    clearGuessInput();
 });
 
 resetButton.addEventListener('click', function () {
-    guessInput.value = '';
+    minGuessInput.value = 0;
+    maxGuessInput.value = 0;
+    clearButton.disabled = true;
+    clearGuessInput();
     clearMessageLines();
     clearError();
-    number = Math.round(Math.random()*101);
+    generateRandomNumber();
 });
 
-// guessInput.addEventListener('keypress', function(){
-// });
+playAgainButton.addEventListener('click', function() {
+  minGuess = parseInt(minGuess) - 10;
+  maxGuess = parseInt(maxGuess) + 10;
+  minGuessInput.value = minGuess;
+  maxGuessInput.value = maxGuess;
+  clearButton.disabled = true;
+  playAgainButton.style.display = 'none';
+  clearGuessInput();
+  clearMessageLines();
+  clearError();
+  generateRandomNumber();
+});
 
 addEventListener('click', function(){
-  // resetButton.disabled = false;
+  if (secondLine.innerText != '' || thirdLine.innerText != '' || errorPrompt.innerText != '') {
+    resetButton.disabled = false;
+  } else {
+    resetButton.disabled = true;
+  };
 });
 
-function disableClearButtonCheck() {
+addEventListener('keyup', function() {
   if (guessInput.value === '') {
     clearButton.disabled = true;
   } else {
     clearButton.disabled = false;
-}}
+  };
+});
 
-setInterval(disableClearButtonCheck, 500);
+minGuessInput.addEventListener('keyup', function(){
+  if (parseInt(minGuessInput.value) != minGuess || parseInt(maxGuessInput.value) != maxGuess) {
+    generateRandomNumber();
+    clearMessageLines();
+    errorPrompt.innerText = '*the random number has been reset';
+  }
+  minGuess = parseInt(minGuessInput.value);
+});
+
+maxGuessInput.addEventListener('keyup', function(){
+  if (parseInt(maxGuessInput.value) != maxGuess) {
+    generateRandomNumber();
+    errorPrompt.innerText = '*the random number has been reset';
+  }
+  maxGuess = parseInt(maxGuessInput.value);
+});
